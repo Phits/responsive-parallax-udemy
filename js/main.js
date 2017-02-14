422eca8017aefeb43af63d0b5168ff7c88826d76
phits <john@fitz.com>
2017-02-14 12:46:26 -0500

    Testimonials finished

--- js/main.js
+++ js/main.js
@@ -70,6 +70,30 @@ jQuery(document).ready(function() {
         }
     });
 
+    $(".testimonial-carousel").carouFredSel({
+        
+        responsive:true,
+        width:'100%',
+        circular:true,
+        scroll:{
+            items:1,
+            duration:500,
+            pauseOnHover: true
+        },
+        auto:false,
+        items:{
+            visible:{
+                min:1,
+                max:1
+            },
+            height:"variable"
+        },
+        pagination: {
+            container: ".testipage",
+            anchorBuilder: false
+        }
+    });
+
     $(window).scroll(function() {
 
         var top = $(window).scrollTop();
